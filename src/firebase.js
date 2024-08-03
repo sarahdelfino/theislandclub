13b74b4b41e117e8df39141d14e8fc3dc1195cd0
Sarah Delfino <sarahdelfino7@gmail.com>
2024-08-02 22:23:01 -0400

    firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { getDatabase } from "firebase/database";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_API_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_API_PROJECT_ID,
-    databaseURL: import.meta.env.VITE_API_DATABASE_URL,
-    storageBucket: import.meta.env.VITE_API_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_API_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_API_APP_ID
-  };
+  apiKey: "AIzaSyC6vSLkiU0Mzb0cpJm7NKlyuNwdnymXqcE",
+  authDomain: "theislandclub.firebaseapp.com",
+  projectId: "theislandclub",
+  databaseURL: "https://theislandclub-default-rtdb.firebaseio.com/",
+  storageBucket: "theislandclub.appspot.com",
+  messagingSenderId: "939559041500",
+  appId: "1:939559041500:web:6e456a66fd2c8852aef719"
+};
 
   const firebase = initializeApp(firebaseConfig);
   // const analytics = getAnalytics(firebaseConfig);
