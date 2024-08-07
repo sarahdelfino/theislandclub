b8c2c344449fca38f283d0cfae25580f8981e3eb
Sarah Delfino <sarahdelfino7@gmail.com>
2024-08-07 17:34:24 -0400

    removed events as child

--- src/views/MembershipView.vue
+++ src/views/MembershipView.vue
@@ -1,9 +1,11 @@
 <template>
     <Membership />
+    <Events />
 </template>
 
 <script setup>
     import Membership from '../components/Membership.vue';
+    import Events from '@/components/Events.vue';
 </script>
 
 <style></style>
