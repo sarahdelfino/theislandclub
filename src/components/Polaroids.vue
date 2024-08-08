9f94665a6bc742b1628918870441519b2b257be8
Sarah Delfino <sarahdelfino7@gmail.com>
2024-08-07 20:35:06 -0400

    test

--- src/components/Polaroids.vue
+++ src/components/Polaroids.vue
@@ -2,7 +2,7 @@
     <div class="polaroids-container">
             <RouterLink to="/about"><img src="../assets/about.svg"></RouterLink>
             <RouterLink to="/membership"><img src="../assets/membership.svg"></RouterLink>
-            <RouterLink to="/donate"><img src="../assets/donate.svg"></RouterLink>
+            <!-- <RouterLink to="/donate"><img src="../assets/donate.svg"></RouterLink> -->
             <RouterLink to="/blog"><img src="../assets/blog.svg"></RouterLink>
     </div>
 </template>
