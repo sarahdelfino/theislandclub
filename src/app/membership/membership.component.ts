eb8b853cfe89f6af8859c2cdfc3b69c1e356d7f2
Sarah Delfino <sarahdelfino7@gmail.com>
2024-08-10 21:32:27 -0400

    new pictures

--- src/app/membership/membership.component.ts
+++ src/app/membership/membership.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { EventsComponent } from '../events/events.component';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-membership',
@@ -22,6 +23,28 @@ export class MembershipComponent {
 toggleRequest() {
   this.signUpVisible = false;
     this.requestFormVisible = !this.requestFormVisible;
+}
+
+submitMembership(e: Event) {
+  e.preventDefault();
+  // submit membership to firebase
+  // console.log("error: ", error.value, " success: ", success.value);
+  emailjs.sendForm("service_66ijhfa", "template_de4aoh4", e.target as HTMLFormElement, {
+      publicKey: 'pp0s7qlmsjt-_40XH',
+  }).then(() => {
+    console.log("huzzah~!")
+      // alertText = 'Your submission has been received!';
+      // success.value = true;
+      // membershipForm.reset();
+      
+
+  },
+      (error) => {
+          // alertText = 'We were not able to process this submission at this time. Please try again.';
+          // error.value = true;
+          console.log('FAILED...', error.value);
+      }
+  );
 }
 
 }
