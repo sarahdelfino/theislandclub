fd88dabb9fc0932a7f10bd3970645baaf97f7189
Sarah Delfino <sarahdelfino7@gmail.com>
2026-09-08 20:57:46 -0400

    home

--- src/app/featured/featured.component.ts
+++ src/app/featured/featured.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { FirebaseService } from '../firebase.service';
 import { AsyncPipe, CommonModule } from '@angular/common';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-featured',
   standalone: true,
-  imports: [AsyncPipe, CommonModule],
+  imports: [AsyncPipe, CommonModule, RouterLink],
   templateUrl: './featured.component.html',
   styleUrl: './featured.component.css'
 })
